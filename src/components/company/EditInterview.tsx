2fabf9f9b627620f5f7831c52f525b3a29e5bd78
Abhinand <abhin4nd6@gmail.com>
2024-09-01 22:19:07 +0530

    fix: fixed some code in login

--- src/components/company/EditInterview.tsx
+++ src/components/company/EditInterview.tsx
@@ -38,7 +38,7 @@ function EditInterview({ ind }: { ind: number }) {
     };
     return (
         <AlertDialog open={open} onOpenChange={handleOpenChange}
-            onClickOutside={(e:any) => e?.preventDefault()}>
+            onClickOutside={(e: any) => e?.preventDefault()}>
             <AlertDialogTrigger asChild className='w-full'>
                 <button onClick={(e) => {
                     e.stopPropagation(); // Prevent event from bubbling up
@@ -127,7 +127,7 @@ function EditScheduleForm({ setOpen, ind }: { setOpen: (pre: boolean) => void, i
                                 <Input
                                     onKeyDown={(e) => e.stopPropagation()}
                                     onClick={(e) => e.stopPropagation()}
-                                    onClick={(e) => e.stopPropagation()} {...field} />
+                                    {...field} />
                             </FormControl>
                             <FormMessage />
                         </FormItem>
@@ -145,8 +145,7 @@ function EditScheduleForm({ setOpen, ind }: { setOpen: (pre: boolean) => void, i
                                 <Input
                                     onKeyDown={(e) => e.stopPropagation()}
                                     onClick={(e) => e.stopPropagation()}
-                                    onClick={(e) => e.stopPropagation()}
-                                      {...field} />
+                                    {...field} />
                             </FormControl>
                             <FormMessage />
                         </FormItem>
